9e72c51c01cc217c17965dbfbed21075bf8c0660
zhaoj <1334561398@qq.com>
2019-10-28 20:28:08 +0800

    给mypage页面加了一个简单的小title
    Signed-off-by: zhaoj <1334561398@qq.com>

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -7,70 +7,106 @@
  */
 
 import React, {Component} from 'react';
-import {
-    StyleSheet,
-    View,
-    Text, Button,
-} from 'react-native';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import actions from '../action';
 import {connect} from 'react-redux';
 import NavigationUtil from '../navigator/NavigationUtil';
+import NavigationBar from '../common/NavigationBar';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const THEME_COLOR = '#678';
 
 class MyPage extends Component<Props> {
+    getRightButton() {
+        return <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+                onPress={() => {
+                }}
+            >
+                <View style={{padding: 5, marginRight: 8}}>
+                    <Feather
+                        name={'search'}
+                        size={24}
+                        style={{color: 'white'}}
+                    />
+                </View>
+
+            </TouchableOpacity>
+        </View>;
+    }
+
+    getLeftButton(callBack) {
+        return <TouchableOpacity
+            style={{padding: 8, paddingLeft: 12}}
+            onPress={callBack}>
+            <Ionicons
+                name={'ios-arrow-back'}
+                size={26}
+                style={{color: 'white'}}/>
+        </TouchableOpacity>;
+    }
+
     render() {
-        const {navigation} = this.props;
+        let statusBar = {
+            backgroundColor: THEME_COLOR,
+            barStyle: 'light-content',
+        };
+        let navigationBar =
+            <NavigationBar
+                title={'我的'}
+                statusBar={statusBar}
+                style={{backgroundColor: THEME_COLOR}}
+                rightButton={this.getRightButton()}
+                leftButton={this.getLeftButton()}
+            />;
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    欢迎来到MyPage
-                </Text>
+                {navigationBar}
+                <Text style={styles.welcome}>MyPage</Text>
                 <Button
                     title="改变主题色"
                     onPress={() => {
-                        // navigation.setParams({
-                        //     theme: {
-                        //         tintColor: 'blue',
-                        //         updateTime: new Date().getTime()
-                        //     }
-                        // })
-                        this.props.onThemeChange('rgba(14,255,255,0.98)');
+                        // let {dispatch} = NavigatorUtil.navigation;
+                        // dispatch(onThemeChange('red'))
+                        this.props.onThemeChange('#62a');
                     }}
                 />
                 <Text onPress={() => {
-                    NavigationUtil.goPage({navigation: this.props.navigation}, 'DetailPage');
+                    NavigationUtil.goPage({
+                        navigation: this.props.navigation,
+                    }, 'DetailPage');
                 }}>跳转到详情页</Text>
-                <Button title={'Fetch使用'} onPress={() => {
-                    NavigationUtil.goPage({
-                        navigation: this.props.navigation,
-                    }, 'FetchDemoPage');
-                }}/>
-                <Button title={'AsyncStorage使用'} onPress={() => {
-                    NavigationUtil.goPage({
-                        navigation: this.props.navigation,
-                    }, 'AsyncStorageDemoPage');
-                }}/>
-                <Button title={'离线缓存框架DataStore使用'} onPress={() => {
-                    NavigationUtil.goPage({
-                        navigation: this.props.navigation,
-                    }, 'DataStoreDemoPage');
-                }}/>
+                <Button
+                    title={'Fetch 使用'}
+                    onPress={() => {
+                        NavigationUtil.goPage({
+                            navigation: this.props.navigation,
+                        }, 'FetchDemoPage');
+                    }}/>
+                <Button
+                    title={'AsyncStorage 使用'}
+                    onPress={() => {
+                        NavigationUtil.goPage({
+                            navigation: this.props.navigation,
+                        }, 'AsyncStorageDemoPage');
+                    }}/>
+                <Button
+                    title={'离线缓存框架'}
+                    onPress={() => {
+                        NavigationUtil.goPage({
+                            navigation: this.props.navigation,
+                        }, 'DataStoreDemoPage');
+                    }}/>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    welcome: {
-        fontSize: 12,
-        textAlign: 'center',
-        margin: 10,
+        marginTop: 30,
     },
 });
 
