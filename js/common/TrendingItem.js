2685062884b794c5f98e6a6c3de51ae68c13ea98
zhaoj <1334561398@qq.com>
2019-11-25 20:32:32 +0800

    9-2 is over , a bug is show !
    Signed-off-by: zhaoj <1334561398@qq.com>

--- js/common/TrendingItem.js
+++ js/common/TrendingItem.js
@@ -2,26 +2,29 @@ import React, {Component} from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import HTMLView from 'react-native-htmlview';
+import BaseItem from './BaseItem';
 
-export default class TrendingItem extends Component {
+export default class TrendingItem extends BaseItem {
     render() {
-        const {item} = this.props;
+        // const {item} = this.props;
+        const {projectModel} = this.props;
+        const {item} = projectModel;
         if (!item) {
             return null;
         }
-        let favoriteButton =
-            <TouchableOpacity
-                style={{padding: 6}}
-                onPress={() => {
-                }}
-                underlayColor={'transparent'}
-            >
-                <FontAwesome
-                    name={'star-o'}
-                    size={26}
-                    style={{color: 'red'}}
-                />
-            </TouchableOpacity>;
+        // let favoriteButton =
+        //     <TouchableOpacity
+        //         style={{padding: 6}}
+        //         onPress={() => {
+        //         }}
+        //         underlayColor={'transparent'}
+        //     >
+        //         <FontAwesome
+        //             name={'star-o'}
+        //             size={26}
+        //             style={{color: 'red'}}
+        //         />
+        //     </TouchableOpacity>;
         let description = '<p>' + item.description + '</p>';
         return (
             <TouchableOpacity
@@ -55,7 +58,8 @@ export default class TrendingItem extends Component {
                             })}
 
                         </View>
-                        {favoriteButton}
+                        {/*{favoriteButton}*/}
+                        {this._favoriteIcon()}
                     </View>
                 </View>
 
